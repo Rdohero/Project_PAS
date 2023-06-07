552a0cd33b9e4169f3256a6f6b7d090e88b816bb
Rdohero <rojaridho8888@gmail.com>
2023-06-07 11:51:46 +0700

    lumayan

--- app/src/main/java/com/example/project_pas/OnboardingActivity.java
+++ app/src/main/java/com/example/project_pas/OnboardingActivity.java
@@ -1,22 +1,38 @@
 package com.example.project_pas;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
 import com.ramotion.paperonboarding.PaperOnboardingFragment;
 import com.ramotion.paperonboarding.PaperOnboardingPage;
 import com.ramotion.paperonboarding.listeners.PaperOnboardingOnRightOutListener;
+
 import java.util.ArrayList;
+
 public class OnboardingActivity extends AppCompatActivity {
     private FragmentManager fragmentManager;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Cek status onboarding
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isFirstTime = sharedPreferences.getBoolean("isFirstTime", true);
+        if (!isFirstTime) {
+            startMainActivity();
+            return;
+        }
+
         setContentView(R.layout.activity_onboarding);
 
         fragmentManager = getSupportFragmentManager();
@@ -28,20 +44,28 @@ public class OnboardingActivity extends AppCompatActivity {
         paperOnboardingFragment.setOnRightOutListener(new PaperOnboardingOnRightOutListener() {
             @Override
             public void onRightOut() {
-                Intent intent = new Intent(OnboardingActivity.this, MainActivity.class);
-                startActivity(intent);
+                // Simpan status onboarding ke shared preferences
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("isFirstTime", false);
+                editor.apply();
+
+                startMainActivity();
             }
         });
     }
 
-
+    private void startMainActivity() {
+        Intent intent = new Intent(OnboardingActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
     private ArrayList<PaperOnboardingPage> getDataforOnboarding() {
         String[] onboardingTitle = getResources().getStringArray(R.array.onboarding_title);
         String[] onboardingDescription = getResources().getStringArray(R.array.onboarding_description);
-        PaperOnboardingPage source1 = new PaperOnboardingPage(onboardingTitle[0], onboardingDescription[0], Color.parseColor("#ffffff"),R.drawable.onboarding_1, R.drawable.ic_search);
-        PaperOnboardingPage source2 = new PaperOnboardingPage(onboardingTitle[1], onboardingDescription[1], Color.parseColor("#ffffff"),R.drawable.onboarding_2, R.drawable.ic_cart);
-        PaperOnboardingPage source3 = new PaperOnboardingPage(onboardingTitle[2], onboardingDescription[2], Color.parseColor("#ffffff"),R.drawable.onboarding_3, R.drawable.ic_payment);
+        PaperOnboardingPage source1 = new PaperOnboardingPage(onboardingTitle[0], onboardingDescription[0], Color.parseColor("#ffffff"),R.drawable.boarding_1, R.drawable.ic_search);
+        PaperOnboardingPage source2 = new PaperOnboardingPage(onboardingTitle[1], onboardingDescription[1], Color.parseColor("#ffffff"),R.drawable.boarding_2, R.drawable.ic_cart);
+        PaperOnboardingPage source3 = new PaperOnboardingPage(onboardingTitle[2], onboardingDescription[2], Color.parseColor("#ffffff"),R.drawable.boarding_3, R.drawable.ic_payment);
         ArrayList<PaperOnboardingPage> elements = new ArrayList<>();
 
         elements.add(source1);
@@ -49,6 +73,5 @@ public class OnboardingActivity extends AppCompatActivity {
         elements.add(source3);
 
         return elements;
-
     }
 }
