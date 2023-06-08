1f62453b42933ea3b9a4eba092ef5e07677f6cf6
FardanAthilla <fardanathilla.gmail.com>
2023-06-08 12:56:21 +0700

    api

--- app/src/main/java/com/example/project_pas/ApiClientSearch.java
+++ app/src/main/java/com/example/project_pas/ApiClientSearch.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 
 public class ApiClientSearch {
     public static Retrofit retrofit;
-    public static final String BASE_URL = "";
+    public static final String BASE_URL = "www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
     public static Retrofit getRetrofitInstance(){
         if (retrofit == null){
             retrofit = new retrofit2.Retrofit.Builder()
